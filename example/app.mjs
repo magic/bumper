b695b6982d8d04a197dfc69d29780f39191d5e5f
Jascha <jascha@jaeh.at>
2020-01-16 21:56:48 +0100

    example/app: update description for line-length

--- example/app.mjs
+++ example/app.mjs
@@ -1,6 +1,11 @@
 export const state = {
   title: '@magic/bumper',
-  description: 'bump package version, git tag, commit and push, npm test, update, and publish, all in one cli script.',
+  description: [
+    'bump package version,',
+    'git tag, commit and push,',
+    'npm test, update, and publish,',
+    'all in one cli script.',
+  ],
   logotext: '@magic/bumper',
   menu: [
     { to: '/#install', text: 'install' },
