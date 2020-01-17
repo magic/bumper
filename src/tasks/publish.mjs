4c132a78b46d1e8bca75912828d69cebad883ea1
Jascha <jascha@jaeh.at>
2020-01-17 21:40:39 +0100

    add publish task, only errors on git diff for now

---
+++ src/tasks/publish.mjs
@@ -0,0 +1,38 @@
+import error from '@magic/error'
+import is from '@magic/types'
+import log from '@magic/log'
+
+import { exec } from '../lib/index.mjs'
+
+import { diff } from './diff.mjs'
+
+const libName = '@magic/bumper.publish'
+
+const expectedChangedFiles = []
+
+export const commit = async state => {
+  state = await diff({ ...state, dangerNoDiff: true })
+
+  const files = state.diff
+
+  if (!is.empty(files)) {
+    const unexpectedFiles = files.filter(f => !expectedChangedFiles.some(e => f.endsWith(e)))
+    if (unexpectedFiles.length) {
+      const msg = `
+${libName} there are uncomitted changes.
+to prevent data corruption, magic-bumper will not work on an unclean workspace.
+
+unexpected changes (new/modified/deleted files):
+${unexpectedFiles.join('\n')}
+
+because the ${libName} task publishes to the npm registry,
+this error can not be ignored using a cli flag.
+      `.trim()
+
+      log.error('E_PRE_NPM_PUBLISH_GIT_DIFF', msg)
+      process.exit(1)
+    }
+  }
+
+  return state
+}
