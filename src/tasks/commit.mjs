c5db9d7a557311da927731489ab58b57d333a416
Jascha <jascha@jaeh.at>
2020-01-17 21:39:20 +0100

    tasks/commit: throws error if git diff shows changes

--- src/tasks/commit.mjs
+++ src/tasks/commit.mjs
@@ -6,10 +6,33 @@ import { exec } from '../lib/index.mjs'
 
 import { diff } from './diff.mjs'
 
-const libName = '@magic/bumper.commit:'
+const libName = '@magic/bumper.commit'
+
+const expectedChangedFiles = ['package.json', 'package-lock.json']
 
 export const commit = async state => {
-  console.log('commit', Object.keys(state))
+  state = await diff({ ...state, dangerNoDiff: true })
+
+  const files = state.diff
+
+  if (!is.empty(files)) {
+    const unexpectedFiles = files.filter(f => !expectedChangedFiles.some(e => f.endsWith(e)))
+    if (unexpectedFiles.length) {
+      const msg = `
+${libName} there are uncomitted changes.
+to prevent data corruption, magic-bumper will not work on an unclean workspace.
+
+unexpected changes (new/modified/deleted files):
+${unexpectedFiles.join('\n')}
+
+because the ${libName} task pushes to a remote git repository,
+this error can not be ignored using a cli flag.
+      `.trim()
+
+      log.error('E_PRE_COMMIT_GIT_DIFF', msg)
+      process.exit(1)
+    }
+  }
 
   return state
 }
