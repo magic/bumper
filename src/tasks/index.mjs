d0e0d4a48157c6117d3875f51d1a8b70c579ca34
Jascha <jascha@jaeh.at>
2020-01-17 02:51:07 +0100

    tasks/index: remove updateDependencies from exports

--- src/tasks/index.mjs
+++ src/tasks/index.mjs
@@ -2,8 +2,7 @@ import { bump as b } from './bump.mjs'
 import { diff as d } from './diff.mjs'
 import { prepare as p } from './prepare.mjs'
 import { test as t } from './test.mjs'
-import { update as u } from './update.mjs'
-import { updateDependencies as uD } from './updateDependencies.mjs'
+import { update as u } from './update/index.mjs'
 import { write as w } from './write.mjs'
 
 export const bump = b
@@ -11,7 +10,6 @@ export const diff = d
 export const prepare = p
 export const test = t
 export const update = u
-export const updateDependencies = uD
 export const write = w
 
 export default {
@@ -20,6 +18,5 @@ export default {
   prepare,
   test,
   update,
-  updateDependencies,
   write,
 }
